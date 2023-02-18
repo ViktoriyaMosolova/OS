e108079326d7bcfa991f3eadd98eefa6a2d6690b
unknown <mosolova.viktoriya@mail.ru>
2023-02-18 17:22:01 +0300

    add lab2

---
+++ lab2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives)
+            {
+                textBox1.Text = Convert.ToString("Drive " + d.Name);
+                textBox1.Text += Convert.ToString("  Drive type: " + d.DriveType);
+                if (d.IsReady == true)
+                {
+                    textBox1.Text = Convert.ToString("  Volume label: " + d.VolumeLabel);
+                    textBox1.Text += Convert.ToString("  File system: " + d.DriveFormat);
+                    textBox1.Text += Convert.ToString("  Available space to current user:" + d.AvailableFreeSpace + " bytes");
+                    textBox1.Text += Convert.ToString("  Total available space:          " + d.TotalFreeSpace + " bytes");
+                    textBox1.Text += Convert.ToString("  Total size of drive:            " + d.TotalSize + " bytes");
+                }
+            }
+
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+
+
+            //1 вывести инфу о названии диска + оперативная память
+            //доп балл - вывод в таймере
+
+            //2 копирование переименование перемещение папок
+
+
+            //ontimer
+
+    }
+}
