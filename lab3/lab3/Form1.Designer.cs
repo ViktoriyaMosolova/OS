18e7dc317702383a1fb8914de9d47a0649b9d719
unknown <mosolova.viktoriya@mail.ru>
2023-02-27 23:04:44 +0300

    add lab3

---
+++ lab3/lab3/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.change = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // change
+            // 
+            this.change.Location = new System.Drawing.Point(578, 157);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(196, 31);
+            this.change.TabIndex = 0;
+            this.change.Text = "Применить изменения";
+            this.change.UseVisualStyleBackColor = true;
+            this.change.Click += new System.EventHandler(this.change_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Не выбрано",
+            "Переименовать",
+            "Копировать",
+            "Переместить",
+            "Создать"});
+            this.comboBox1.Location = new System.Drawing.Point(98, 163);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(234, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Что сделать?";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(98, 88);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(474, 43);
+            this.listBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Файл",
+            "Папка"});
+            this.comboBox2.Location = new System.Drawing.Point(98, 33);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(234, 21);
+            this.comboBox1.SelectedItem = 0;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(95, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 21);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Файл или папка";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(575, 88);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 43);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Удалить выбранный файл или папку";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(338, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 29);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Добавить путь";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(25, 243);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(747, 195);
+            this.splitContainer1.SplitterDistance = 365;
+            this.splitContainer1.TabIndex = 16;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(365, 195);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(2, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(373, 195);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(95, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(450, 21);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Выберите файл или папку нажав кнопку \"Добавить путь\"";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(25, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(322, 21);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Окно 1 - Выберите папку, чтобы открыть содержимое";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(393, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 21);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Окно 2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(346, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 20);
+            this.textBox1.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(343, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 21);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Поле ввода";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.change);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button change;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private Label label2;
+        private Label label4;
+        private Label label5;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private Label label6;
+    }
+}
+
---
+++ lab3/lab3/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox2.SelectedIndex = 0;
+            comboBox1.SelectedIndex = 0;
+            PopulateTreeView();
+        }
+
+        //окна 1 и 2 -----------------------------------------------------------------
+        private void PopulateTreeView()
+        {
+            TreeNode rootNode;
+            DirectoryInfo info = new DirectoryInfo("C:\\Users\\1\\test");
+            if (info.Exists)
+            {
+                rootNode = new TreeNode(info.Name);
+                rootNode.Tag = info;
+                GetDirectories(info.GetDirectories(), rootNode);
+                treeView1.Nodes.Add(rootNode);
+            }
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                aNode.ImageKey = "folder";
+                subSubDirs = subDir.GetDirectories();
+                if (subSubDirs.Length != 0)
+                {
+                    GetDirectories(subSubDirs, aNode);
+                }
+                nodeToAddTo.Nodes.Add(aNode);
+            }
+        }
+        
+        void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode newSelected = e.Node;
+            listView1.Items.Clear();
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+
+            foreach (DirectoryInfo dir in nodeDirInfo.GetDirectories())
+            {
+                item = new ListViewItem(dir.Name, 0);
+                subItems = new ListViewItem.ListViewSubItem[]
+                    {new ListViewItem.ListViewSubItem(item, "Directory"),
+             new ListViewItem.ListViewSubItem(item,
+                dir.LastAccessTime.ToShortDateString())};
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+            foreach (FileInfo file in nodeDirInfo.GetFiles())
+            {
+                item = new ListViewItem(file.Name, 1);
+                subItems = new ListViewItem.ListViewSubItem[]
+                    { new ListViewItem.ListViewSubItem(item, "File"),
+             new ListViewItem.ListViewSubItem(item,
+                file.LastAccessTime.ToShortDateString())};
+
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+        //----------------------------------------------------------------------------
+        
+        
+        //кнопка добавить путь. добавляет путь к файлу или папке в зависимости от выбранного элемента в combobox1
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var filePath = string.Empty;
+            var folderPath = string.Empty;
+            if (comboBox2.SelectedIndex == 0)//file
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        filePath = openFileDialog.FileName;
+                    }
+                }
+                listBox1.Items.Add(filePath);
+                listBox1.SelectedItem = filePath;
+            }
+            else if (comboBox2.SelectedIndex == 1)//folder
+            {
+                DialogResult result = folderBrowserDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    folderPath = folderBrowserDialog1.SelectedPath;
+                }
+                listBox1.Items.Add(folderPath);
+                listBox1.SelectedItem = folderPath;
+            }
+        }
+        //----------------------------------------------------------------------------
+
+        //----------------------------------------------------------------------------
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1) //"rename",
+            {
+                textBox1.Text = "newname";
+                MessageBox.Show("Введите в поле ввода новое имя файла или папки");
+            } 
+            else if (comboBox1.SelectedIndex == 2) //"copy",
+            {
+                textBox1.Text = Path.GetDirectoryName(listBox1.SelectedItem.ToString());
+                MessageBox.Show("Введите в поле ввода путь к папке в которой нужно сохранить скопированный файл или папку");
+            }
+            else if (comboBox1.SelectedIndex == 3) //"move",
+            {
+                textBox1.Text = listBox1.SelectedItem.ToString();
+                MessageBox.Show("Введите в поле ввода новый полный путь до файла или папки");
+            }
+            else if (comboBox1.SelectedIndex == 4) //create
+            {
+                textBox1.Text = "new_element.txt";
+                MessageBox.Show("Введите в поле ввода имя файла с расширением. Файл создастся в выбранной папке или в папке выбранного файла");
+            }
+        }
+        //----------------------------------------------------------------------------
+
+        void CopyDir(string FromDir, string ToDir)
+        {
+            Directory.CreateDirectory(ToDir);
+            foreach (string s1 in Directory.GetFiles(FromDir))
+            {
+                string s2 = ToDir + "\\" + Path.GetFileName(s1);
+                File.Copy(s1, s2);
+            }
+            foreach (string s in Directory.GetDirectories(FromDir))
+            {
+                CopyDir(s, ToDir + "\\" + Path.GetFileName(s));
+            }
+        }
+
+
+
+        //удаление файлов и папок
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int selind = listBox1.SelectedIndex;
+                string Path = listBox1.Items[selind].ToString();
+                if (comboBox2.SelectedIndex == 0)
+                {
+                    if (File.Exists($"{Path}"))
+                    {
+                        File.Delete($"{Path}");
+                        MessageBox.Show("Файл удален");
+                    }
+                }
+                else if (comboBox2.SelectedIndex == 1)
+                {
+                    if (Directory.Exists($"{Path}"))
+                    {
+                        DirectoryInfo dirInfo = new DirectoryInfo($"{Path}");
+                        foreach (FileInfo f in dirInfo.GetFiles())
+                        {
+                            f.Delete();
+                        }
+                        Directory.Delete($"{Path}");
+                        MessageBox.Show("Папка удалена");
+                    }
+                }
+                listView1.Items.Clear();
+                treeView1.Nodes.Clear();
+                PopulateTreeView();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка удаления");
+            }
+
+        }
+
+        private void change_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1) //"rename"
+            {
+                try
+                {
+                    if (comboBox2.SelectedIndex == 0)
+                    {
+                        string directory = Path.GetDirectoryName(listBox1.SelectedItem.ToString());
+                        directory += '\\';
+                        directory += textBox1.Text;
+                        directory += Path.GetExtension(listBox1.SelectedItem.ToString());
+                        File.Move(listBox1.SelectedItem.ToString(), $"{directory}");
+                        MessageBox.Show("Файл переименован");
+                    }
+                    else if (comboBox2.SelectedIndex == 1)
+                    {
+                        string directory = Path.GetDirectoryName(listBox1.SelectedItem.ToString());
+                        directory += '\\';
+                        directory += textBox1.Text;
+                        directory += Path.GetExtension(listBox1.SelectedItem.ToString());
+                        Directory.Move(listBox1.SelectedItem.ToString(), directory);
+                        MessageBox.Show("Папка переименована");
+                    }
+                    comboBox1.SelectedIndex = 0;
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка переименования");
+                }
+            }
+            else if (comboBox1.SelectedIndex == 2) //"copy",
+            {
+                try
+                {
+                    if (comboBox2.SelectedIndex == 0)
+                    {
+                        string name = textBox1.Text + '\\' + "copy_" +Path.GetFileName(listBox1.SelectedItem.ToString());
+                        File.Copy(listBox1.SelectedItem.ToString(), name);
+                        MessageBox.Show("Файл скопирован");
+                    }
+                    else if (comboBox2.SelectedIndex == 1)
+                    {
+
+                        string s = new DirectoryInfo(listBox1.SelectedItem.ToString()).Name;
+                        string name = textBox1.Text + '\\' + "copy_" + s;
+                        CopyDir(listBox1.SelectedItem.ToString(), name);
+                        MessageBox.Show("Папка скопирована");
+                    }
+                    comboBox1.SelectedIndex = 0;
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка копирования");
+                }
+            }
+            else if (comboBox1.SelectedIndex == 3) //"move",
+            {
+                try
+                {
+                    if (comboBox2.SelectedIndex == 0)
+                    {
+                        File.Move(listBox1.SelectedItem.ToString(), textBox1.Text);
+                        MessageBox.Show("Файл перемещен");
+                    }
+                    else if (comboBox2.SelectedIndex == 1)
+                    {
+                        Directory.Move(listBox1.SelectedItem.ToString(), textBox1.Text);
+                        MessageBox.Show("Папка перемещена");
+                    }
+                    comboBox1.SelectedIndex = 0;
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка перемещения");
+                }
+            }
+            else if (comboBox1.SelectedIndex == 4) //create
+            {
+                try
+                {
+                    if (comboBox2.SelectedIndex == 0)
+                    {
+                        string directory = Path.GetDirectoryName(listBox1.SelectedItem.ToString());
+                        directory += '\\';
+                        directory += textBox1.Text;
+                        File.Create(directory);
+                        MessageBox.Show("Файл создан");
+                    }
+                    else if (comboBox2.SelectedIndex == 1)
+                    {
+                        string directory = listBox1.SelectedItem.ToString();
+                        directory += '\\';
+                        directory += textBox1.Text;
+                        File.Create(directory);
+                        MessageBox.Show("Файл создан");
+                    }
+                    comboBox1.SelectedIndex = 0;
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка создания");
+                }
+            }
+            listView1.Items.Clear();
+            treeView1.Nodes.Clear();
+            PopulateTreeView();
+        }
+        //----------------------------------------------------------------------------
+    }
+}
