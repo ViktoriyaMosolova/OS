a6e829ee03a30f5948b98e0c931e276953fc5152
unknown <mosolova.viktoriya@mail.ru>
2023-03-18 03:22:33 +0300

    add lab5

---
+++ lab5/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 103);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(324, 291);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 10F);
+            this.button2.Location = new System.Drawing.Point(318, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Закрыть";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Получить информацию о дисках",
+            "Открыть проводник",
+            "Получить логические диски"});
+            this.listBox1.Location = new System.Drawing.Point(63, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 43);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.listBox1_SelectedValueChanged);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(63, 103);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(662, 291);
+            this.treeView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ lab5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using ClassLibrary1;
+using ClassLibrary2;
+using Timer = System.Threading.Timer;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+
+        public static Timer tmr1, tmr3;
+        public readonly Thread InfoDrive;
+        public readonly Thread Exp;
+        public readonly Thread str3;
+        public Form1()
+        {
+            InitializeComponent(); 
+            textBox1.Visible = false; 
+            button2.Visible = false;
+            treeView1.Visible = false;
+            InfoDrive = new Thread(Cat);
+            Exp = new Thread(Alpaca);
+            str3 = new Thread(Capibara);
+        }
+
+        public void listBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            button2.Visible = true;
+            treeView1.Visible = false;
+            try
+            {
+                tmr1.Dispose();
+                tmr3.Dispose();
+            }
+            catch
+            {
+
+            }
+            if (listBox1.SelectedIndex == 0)
+            {
+                textBox1.Visible = true;
+                tmr1 = new Timer(Cat, "tick1...", 0, 100);
+
+            }
+            if (listBox1.SelectedIndex == 1)
+            {
+                treeView1.Visible = true;
+                treeView1.Nodes.Clear();
+                Alpaca();
+            }
+            if (listBox1.SelectedIndex == 2)
+            {
+                textBox1.Visible = true;
+                tmr3 = new Timer(Capibara, "tick3...", 0, 100);
+            }
+        }
+
+        void Cat(object data)
+        {
+            textBox1.Text = "";
+            Action showMethod = delegate () { foreach (string item in InfoAboutDrive.OutputInfoAboutDrive()) { textBox1.Text += item; }};
+            textBox1.BeginInvoke(showMethod);
+        }
+
+        void Alpaca()
+        {
+            treeView1.BeginInvoke((Action)(() => FileExplorer.OpenFileExplorer(treeView1)));
+        }
+        void Capibara(object data)
+        {
+            textBox1.Text = "";
+            Action showMethod = delegate () { foreach (string item in LogicalDrives.OutputGetLogicalDrives()) { textBox1.Text += item; } };
+            textBox1.BeginInvoke(showMethod);
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = false;
+            button2.Visible = false;
+            treeView1.Visible = false;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            InfoDrive.Abort();
+            Exp.Abort();
+            str3.Abort();
+        }
+    }
+}
