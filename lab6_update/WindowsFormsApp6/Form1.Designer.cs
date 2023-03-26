d451e32c9bbf5b3a4c868af9fec7e389efc2d3a8
unknown <mosolova.viktoriya@mail.ru>
2023-03-26 15:20:11 +0300

    add lab6_update

---
+++ lab6_update/WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.update = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.rename = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // update
+            // 
+            this.update.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.update.Location = new System.Drawing.Point(663, 25);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(125, 42);
+            this.update.TabIndex = 0;
+            this.update.Text = "Обновить окна";
+            this.update.UseVisualStyleBackColor = true;
+            this.update.Click += new System.EventHandler(this.update_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(209, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(433, 394);
+            this.listBox1.TabIndex = 1;
+            // 
+            // rename
+            // 
+            this.rename.Location = new System.Drawing.Point(57, 26);
+            this.rename.Name = "rename";
+            this.rename.Size = new System.Drawing.Size(125, 41);
+            this.rename.TabIndex = 2;
+            this.rename.Text = "Переименовать";
+            this.rename.UseVisualStyleBackColor = true;
+            this.rename.Click += new System.EventHandler(this.rename_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(663, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Kill me...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rename);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.update);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button rename;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ lab6_update/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        Process[] Gprocesseslist;
+        List<IntPtr> ListName = new List<IntPtr>();
+        delegate bool EnumThreadDelegate(IntPtr hWnd, IntPtr lParam);
+
+        [Flags]
+        private enum ProcessAccessFlags : uint
+        {
+            QueryLimitedInformation = 0x00001000
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowTextA(IntPtr hWnd, string text);
+        [DllImport("user32.dll")]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        static extern bool GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        [DllImport("user32.dll")]
+        static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childafter, string lclass, string WindowTitle);
+        [DllImport("user32.dll")]
+        static extern bool EnumThreadWindows(int dwThreadId, EnumThreadDelegate lpfn, IntPtr lParam);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool QueryFullProcessImageName(
+        [In] IntPtr hProcess,
+        [In] int dwFlags,
+        [Out] StringBuilder lpExeName,
+        ref int lpdwSize);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, int processId);
+
+        static List<IntPtr> EnumProcessWinHandle(Process process)
+        {
+            var handles = new List<IntPtr>();
+            foreach (ProcessThread thread in process.Threads)
+                EnumThreadWindows(thread.Id,(hWnd, lParam) => { handles.Add(hWnd); return true; }, IntPtr.Zero);
+            return handles;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            UpdatePage();
+        }
+
+        private void UpdatePage()
+        {
+            listBox1.Items.Clear();
+            Gprocesseslist = Process.GetProcesses();
+            ListName.Clear();
+            foreach (Process process in Gprocesseslist)
+            {
+                if (process.ProcessName != "Idle")
+                {
+                    ListName.AddRange(EnumProcessWinHandle(process));
+                }
+            }
+            for (int i = ListName.Count - 1; i >= 0; i--)
+            {
+                if (GetWindowTextLength(ListName[i]) == 0)
+                {
+                    ListName.RemoveAt(i);
+                }
+            }
+            foreach (IntPtr handle in ListName)
+            {
+                StringBuilder window_text = new StringBuilder(GetWindowTextLength(handle) + 1);
+                GetWindowText(handle, window_text, window_text.Capacity);
+                listBox1.Items.Add(window_text.ToString());
+            }
+        }
+
+        private void rename_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                SetWindowTextA(ListName[listBox1.SelectedIndex], textBox1.Text);
+                UpdatePage();
+                textBox1.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("FFFFFFFFFFF");
+            }
+        }
+        
+        private void update_Click_1(object sender, EventArgs e)
+        {
+            UpdatePage();
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+
+        const UInt32 WM_CLOSE = 0x0010;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SendMessage(ListName[listBox1.SelectedIndex], WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
